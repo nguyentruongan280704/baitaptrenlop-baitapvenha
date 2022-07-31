66dc79b3419d9abf3bf76e2e7b5a7b0999b87a51
nguyentruongan280704 <110025394+nguyentruongan280704@users.noreply.github.com>
2022-08-01 00:58:42 +0700

    Add files via upload

---
+++ baitap29-7/appbaitap29-7.js
@@ -0,0 +1,124 @@
+// phần 1
+// 1 var , let ,const la kiểu câu lệnh khai báo biến
+// 2  khai báo là phạm vi toàn cầu hoặc phạm vi chức năng trong khi let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// phần 2 
+// bai1 
+// let a = Number(prompt("nhap so vao day"));
+// if (a > 0 && a < 4.5){
+//     alert("lowerhalf");   
+// }else if( a >= 4.5 && a <9){
+//     alert("highhalf")
+// }else alert("khonog thuoc khoang nao ca");
+
+// bai2
+// let n = Number(prompt("nhap xo n vao"));
+// let x = Number(prompt("nhap so x vao "));
+// if(x > 0 && x < n/2){
+//     alert("nho hon");
+// }else if(x > n/2 && x > n){
+//     alert("lon hon");
+// }else alert("khong thuoc khoang nao");
+
+// bai 3
+// let x = Number(prompt("nhap so s"));
+// let phepchialaydu = x % 2
+// if (phepchialaydu === 0){
+//     alert("so chan");
+// }else alert("so le")
+
+// bai 4
+// let a = Number(prompt("so can nang"));
+// let b = Number(prompt("nhap chieu cao"));
+// let BMI = a / (b * b)
+// if(BMI < 16){
+//     alert("may la bo suong a");
+// }else if (BMI > 16 && BMI < 18.5){
+//     alert("may la idol han quoc a");
+// }else if (BMI > 18.5 && BMI < 25){
+//     alert("may dep zai day");
+// }else if (BMI > 25 && BMI < 30){
+//     alert("may beo qua roi");
+// }else if (BMI > 30){
+//     alert("thoi may k can giam beo dau");
+
+// }
+
+// bai 5 
+// let a = Number(prompt("diem toan"));
+// let b = Number(prompt("diem ly"));
+// let c = Number(prompt("diem hoa"));
+// let d = Number(prompt("diem van"));
+// let e = Number(prompt("diem anh"));
+// let tong = a + b + c + d + e
+// let phantram = tong/50 * 100
+// if (phantram >= 90){
+//     alert("xephang a");
+// }else if (phantram >= 80){
+//     alert("xephang b");
+// }else if (phantram >= 70){
+//     alert("xephang c");
+// }else if (phantram >= 60){
+//     alert("xephang d");
+// }else if (phantram >= 40){
+//     alert("xephang e");
+// }else {
+//     alert("xephang f");
+// }
+
+// bai 6
+// let a = Number(prompt("nhap nam"));
+// let  b = a % 4 
+// if (  a > 0 && b === 0 ){
+//     alert("nam do la nam nhuan");
+// }else alert("nam do la nam khong nhuan");
+ 
+// bai 7 
+// let a = Number(prompt("nhapchieucao(cm)"));
+// if (a < 150){
+// alert("may la ng lun a");
+// }else if (a > 150 && a < 169){
+//     alert("trong m duoc");
+// }else if (a >= 17){
+//     alert("m trong duoc day");
+// }
+
+
+// bai 8 
+// let a = Number(prompt("nhap"));
+// let b = Number(prompt("nhap"));
+// if (a > b){
+//     console.log("thi a lon hon");
+// }else console.log("thi b lon hon");
+
+
+// bai 10
+// let a = Number(prompt("nhap so a"));
+// let b = Number(prompt("nhap so b"));
+// let c = Number(prompt("nhap so c"));
+// if (a > b && a >c) {
+//     alert("a la so lon nhat");
+// }else if (b > a && b > c){
+//     alert("b la so lon nhat");
+// }else if (c > a && c > b){
+//     alert ("c la so lon nhat");
+// }
+
+// bai 11 
---
+++ baitap29-7/baitapvenha31/appbaitap31.js
@@ -0,0 +1,71 @@
+// // bai 1
+// let a = Number(prompt("nhap so a"));
+// let b = Number(prompt("nhap so b"));
+// if (a % b === 0) {
+//     alert("a chia het cho b");
+// }else alert  ("a k chia het cho b");
+
+// bai 2
+// let a = Number(prompt("nhap tuoi"));
+// if (a < 15){
+//     alert ("k du dk");
+// }else if ( a = 15){
+//     alert ("du dk");
+// }
+
+//  bai 3 
+// let a = Number(prompt("nhap so nguyen"));
+// if ( a > 0 && a > 0){
+//     alert ("a lon hon");
+// }else alert ("a nho hon");
+
+// bai 4 
+// let a = Number(prompt("nhap so a"));
+// let b = Number(prompt("nhap so b"));
+// let c = Number(prompt("nhap so c"));
+// if (a > b && a > c){
+//     alert("a lon nhat");
+// }else if (b > a && b > c){
+//     alert("b lon nhat");
+// }else if (c > a && c > b){
+//     alert("c lon nhat");
+// }
+
+// bai 7 
+// let a = Number(prompt("nhap can nang"));
+// let b = Number(prompt("nhap chieu cao"));
+// let BMI = a / (b ^ 2);
+// if (BMI < 18 ){
+// alert("thieu can");
+// }else if (BMI < 25){
+//     alert("binh thuong");
+// }else if (BMI < 30){
+//     alert("thua can");
+// }else alert("beo qua");
+
+// bai 5
+// let a = Number(prompt("diem bai kt"));
+// let b = Number(prompt("diem bai giua ki"));
+// let c = Number(prompt("diem bai cuoi ki"));
+// let tong = (a + b + c) /3
+// if (tong >= 3.5 && tong <= 4.9 ){
+//     alert("yeu");
+// }else if (tong >= 5 && tong <= 6.5){
+//     alert("tb");
+// }else if (tong >= 6.5 && tong <= 8){
+//     alert("kha");
+// }else if (tong >= 8){
+//     alert("la gioi");
+// }
+
+
+// bai 6
+let a = Number(prompt("muc doanh so ban hang (trieu)"));
+if (hang <= 100){
+    alert("se nhan duoc 5 phan chan hh hang thang");
+}else if (100 < hang && hang <= 300){
+    alert("se nhan dc 10 phan cham hoa hong hang thang");
+}else alert("se nhan duoc 20 phan cham hang thang");
+
+
+
